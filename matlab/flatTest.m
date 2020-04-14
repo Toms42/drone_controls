23f44a9c763525aacdc51ecdaef0a3e4b81d0286
Toms42 <tomscherlis@gmail.com>
2020-04-13 20:42:26 -0400

    add feedforward

--- matlab/flatTest.m
+++ matlab/flatTest.m
@@ -1,3 +1,4 @@
+close all
 dt = 0.05;
 
 A = [zeros(3), eye(3), zeros(3,1);
@@ -9,7 +10,7 @@ B = [zeros(3), zeros(3,1);
 
 g = 9.81;
 G = [0 0 0 0 0 -1 0]' * g;
-G_ff = [0 0 1 0]' * g;
+%G_ff = [0 0 1 0]' * g;
 Q = diag([10,10,10,.1,.1,.1,1]);
 R = diag([.1, .1, .1, .1]);
 
@@ -20,9 +21,10 @@ v0 = zeros(3,1);
 a0 = zeros(3,1);
 v1 = zeros(3,1);
 z1 = zeros(3,1);
-[xx, yy, zz, vxx, vyy, vzz] = constructMinimumSnapTraj(dt,waypts,v0,a0,v1,a1);
+[xx, yy, zz, vxx, vyy, vzz, axx, ayy, azz] = constructMinimumSnapTraj(dt,waypts,v0,a0,v1,a1);
 psi = atan2(vyy,vxx);
 ref_traj = [xx;yy;zz;vxx;vyy;vzz;psi];
+feedforward = [axx; ayy; azz + ones(size(axx)) * g; zeros(size(axx))];
 vis.setReferenceTraj(ref_traj);
 
 x = [0 0 0 0 0 0 0]';
@@ -32,8 +34,13 @@ for n = 1:size(ref_traj,2)+1/dt
     tic
     
     x_ref = ref_traj(:,min(n,size(ref_traj,2)));
+    if n > size(feedforward,2)
+        ff = zeros(4,1);
+    else
+        ff = feedforward(:,n);
+    end
     
-    u = -k*(x - x_ref) + G_ff;
+    u = -k*(x - x_ref) + ff;
     x = x + dt*(A*x + B*u + G);
     
     vis.setRobotState(x(1:3),eul2rotm([x(7),0,0],'zyx'),n);
